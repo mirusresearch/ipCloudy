e9165cfda0ae5a11284bd07b0a10999e50b233c4
EyePulp <eyepulp@eyepulp.net>
2019-07-24 00:12:29 -0500

    catch whois errors

--- src/providers/whois.js
+++ src/providers/whois.js
@@ -8,8 +8,16 @@ const lookup = Promise.promisify(whois.lookup);
 const entryRegex = /^(network:)?Organization(;I)?:\W*(.*)/;
 
 module.exports = async function(ip) {
-    let data = await lookup(ip);
+    let data;
+    try {
+        data = await lookup(ip);
+    } catch (error) {
+        console.error('ipCloudy:', error);
+        return null;
+    }
     let org = data.split('\n').filter(entry => entryRegex.test(entry));
-    if (org.length) return org[0].replace(entryRegex, '$3');
+    if (org.length) {
+        return org[0].replace(entryRegex, '$3');
+    }
     return null;
 };
